970cf5199981a2b9fda6e40523a43cd85f149ed1
Ayemen27 <Ayemen28197@gmail.com>
2025-12-06 22:48:50 +0000

    نشر تلقائي 2025-12-06 22:48:50

--- app2/vite.config.ts
+++ app2/vite.config.ts
@@ -26,9 +26,6 @@ export default defineConfig({
       output: {
         manualChunks: (id) => {
           if (id.includes('node_modules')) {
-            if (id.includes('react') || id.includes('react-dom')) {
-              return 'vendor-react';
-            }
             if (id.includes('@radix-ui')) {
               return 'vendor-radix';
             }

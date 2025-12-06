141805cc77784ef566f673fe3d1b74b3e72dc9e2
Ayemen27 <Ayemen28197@gmail.com>
2025-12-06 22:18:47 +0000

    نشر تلقائي 2025-12-06 22:18:47

--- app2/server/index.ts
+++ app2/server/index.ts
@@ -258,17 +258,14 @@ app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
   throw err;
 });
 
-// importantly only setup vite in development and after
-// setting up all the other routes so the catch-all route
-// doesn't interfere with the other routes
-(async () => {
-  if (app.get("env") === "development") {
-    const { setupVite } = await import("./vite");
-    await setupVite(app, server);
-  } else {
-    serveStatic(app);
-  }
-})();
+// Setup static files or vite dev server based on environment
+if (process.env.NODE_ENV === "development") {
+  import("./vite").then(({ setupVite }) => {
+    setupVite(app, server);
+  });
+} else {
+  serveStatic(app);
+}
 
 // ALWAYS serve the app on the port specified in the environment variable PORT
 // Other ports are firewalled. Default to 5000 if not specified.

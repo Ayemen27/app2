a149a8f103b9440dc146fdb2b52330789a877cbe
Ayemen27 <ayemen28197@gmail.com>
2026-02-26 02:24:54 +0000

    ر

--- android/app/src/main/java/com/axion/app/MonitoringApplication.java
+++ android/app/src/main/java/com/axion/app/MonitoringApplication.java
@@ -1,16 +1,18 @@
 package com.axion.app;
 
 import androidx.multidex.MultiDexApplication;
-import io.sentry.android.core.SentryAndroid;
+// import io.sentry.android.core.SentryAndroid;
 
 public class MonitoringApplication extends MultiDexApplication {
     @Override
     public void onCreate() {
         super.onCreate();
+        /*
         SentryAndroid.init(this, options -> {
             options.setDsn("https://examplePublicKey@o0.ingest.sentry.io/0");
             options.setTracesSampleRate(1.0);
             options.setDebug(false);
         });
+        */
     }
 }
